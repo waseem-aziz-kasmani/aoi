c5785f9eb30ce6812a3c857a49ea2660388441af
Waseem Kasmani <154504249+waseem-aziz-kasmani@users.noreply.github.com>
2025-05-26 17:23:46 +0500

    Create JS-CLICK.js

---
+++ JS-CLICK.js
@@ -0,0 +1,44 @@
+function D2_1(){  
+    var x = document.getElementById("BX1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function D2_2(){  
+    var x = document.getElementById("BX2");
+  if (x.style.display === "none") {
+    PriceUpdate_1();
+    x.style.display = "block";
+    
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function D2_3(){  
+    var x = document.getElementById("BX3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function D2_4(){  
+    var x = document.getElementById("BX4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function D2_5(){  
+     var x = document.getElementById("BX5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }    
+}
